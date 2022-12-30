fc547a54bed1d8a84c748ce3000a04b1e0e394fb
Andrei Marin <hedrox53@gmail.com>
2022-12-30 18:58:16 +0200

    Add day4

---
+++ 2022/day4/day4b.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	var noContained int
+	file, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		txt := scanner.Text()
+		text := strings.Split(txt, ",")
+		var lhs0, lhs1, rhs0, rhs1 int
+		fmt.Sscanf(text[0], "%d-%d", &lhs0, &lhs1)
+		fmt.Sscanf(text[1], "%d-%d", &rhs0, &rhs1)
+
+		if lhs0 <= rhs1 && lhs1 >= rhs0 || rhs0 <= lhs1 && rhs1 >= lhs0 {
+			noContained++
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+	fmt.Println(noContained)
+}
